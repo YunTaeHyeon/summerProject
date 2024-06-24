a0ac7baad7b3e19e426edb095f7d4ddc9d9c9ede
t05h19@naver.com <t05h19@naver.com>
2024-06-24 22:22:50 +0900

    feat: start_screen.dart로 첫 페이지 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screen/login_screen.dart';
 import 'screen/welcome_page.dart';
+import 'screen/start_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      title: 'Welcome Page',
-      home: WelcomePage(),
+      title: 'Start Page',
+      home: StartScreen(),
     );
   }
 }
