967932105c18afbdd2185e8147581c83431ec2e1
t05h19@naver.com <t05h19@naver.com>
2024-06-24 22:23:41 +0900

    feat: 카테고리 tabbar 사용

--- lib/screen/next_page.dart
+++ lib/screen/next_page.dart
@@ -5,16 +5,62 @@ class NextPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Next Page'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Next Page!',
-          style: TextStyle(fontSize: 24.0),
+    return DefaultTabController(
+      length: 9, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('실시간'),
+          elevation: 0.0,
+          backgroundColor: Colors.grey[800],
+          centerTitle: true,
+          // 왼쪽 상단에 메뉴바
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: '전체'),
+              Tab(text: '연예'),
+              Tab(text: '스포츠'),
+              Tab(text: '정치'),
+              Tab(text: '경제'),
+              Tab(text: '사회'),
+              Tab(text: '생활/문화'),
+              Tab(text: 'IT/과학'),
+              Tab(text: '세계'),
+            ],
+          ),
         ),
+        body: TabBarView(
+          children: [
+            buildCategoryPage('카테고리 1'),
+            buildCategoryPage('카테고리 2'),
+            buildCategoryPage('카테고리 3'),
+            buildCategoryPage('카테고리 4'),
+            buildCategoryPage('카테고리 5'),
+            buildCategoryPage('카테고리 6'),
+            buildCategoryPage('카테고리 7'),
+            buildCategoryPage('카테고리 8'),
+            buildCategoryPage('카테고리 9'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCategoryPage(String category) {
+    return Center(
+      child: Text(
+        '게시물 받아올 예정 - $category',
+        style: TextStyle(fontSize: 24.0),
       ),
     );
   }
